ba3563a8caff6f5969c8a570dc23dfe65f42f4c7
klu2300032597 <2300032597@kluniversity.in>
2025-09-23 01:48:00 +0530

    updated frontend

--- FRONTEND/FSDCLIENT/src/farmer/FarmerNavbar.jsx
+++ FRONTEND/FSDCLIENT/src/farmer/FarmerNavbar.jsx
@@ -18,7 +18,7 @@ import FarmerHome from './FarmerHome';
 import AddProducts from './AddProducts';
 import MyListings from './MyListings';
 import Transactions from './../buyer/Transactions';
-import './farmercss/farmer.css'; 
+import '../farmer/farmercss/farmer.css'; 
 import { useAuth } from '../contextapi/AuthContext';
 import FarmerProfile from './FarmerProfile';
 import logo from '../assets/logo.png';
