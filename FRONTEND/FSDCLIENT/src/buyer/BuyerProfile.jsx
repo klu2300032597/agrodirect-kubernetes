302c4f6c49f6f0b2b590589cc2692b6b80ade155
klu2300032597 <2300032597@kluniversity.in>
2025-09-23 02:22:31 +0530

    updated frontend

--- FRONTEND/FSDCLIENT/src/buyer/BuyerProfile.jsx
+++ FRONTEND/FSDCLIENT/src/buyer/BuyerProfile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './buyercss/BuyerProfile';
+import './buyercss/BuyerProfile.css';
 
 export default function BuyerProfile() {
   const [buyer, setBuyer] = useState(null);
